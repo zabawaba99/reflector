56648ea5fa6addadb7d0e0bddc0a571bb98f8542
Steven Berlanga <zabawaba99@gmail.com>
2015-01-22 20:45:14 -0500

    initial commit

---
+++ src/main/java/com/zabawaba/reflector/ReflectionUtil.java
@@ -0,0 +1,11 @@
+package com.zabawaba.reflector;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+
+public class ReflectionUtil {
+
+	public static ArrayList<Method> getMethods(Class<?> clazz) {
+		return null;
+	}
+}
