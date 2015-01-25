cd870870c6b6c0b5d7cbcfa323a2eb4bf0221471
Steven Berlanga <zabawaba99@gmail.com>
2015-01-25 09:35:05 -0500

    adding method to fetch all fields #2

--- src/test/java/com/zabawaba/reflector/classes/SampleOne.java
+++ src/test/java/com/zabawaba/reflector/classes/SampleOne.java
@@ -1,6 +1,9 @@
 package com.zabawaba.reflector.classes;
 
-public class Methods {
+public class SampleOne {
+	public String field1;
+	public int field2;
+
 	public void method1(){}
 	public void method2(){}
 }
