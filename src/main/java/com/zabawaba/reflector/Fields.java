d7f4171d5d81ba1d8e0969f82e5ed4d532f378dd
Steven Berlanga <zabawaba99@gmail.com>
2015-02-15 15:00:04 -0500

    converting the static methods on Fields to instance methods to reduce the number of parameters that are needed to list/get fields

--- src/main/java/com/zabawaba/reflector/Fields.java
+++ src/main/java/com/zabawaba/reflector/Fields.java
@@ -4,6 +4,11 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.util.HashSet;
 
+/**
+ * Provides a set methods to access and manipulate {@link Field}s
+ * 
+ * @author zabawaba
+ */
 public class Fields {
 	private static Filter<Field> ALL_FIELDS = new Filter<Field>() {
 		public boolean apply(Field field) {
@@ -39,11 +44,61 @@ public class Fields {
 	 *             If no field exists with the provided name
 	 */
 	public ReflectorField get(String fieldName) throws NoSuchFieldException {
-		Field field = getField(obj.getClass(), fieldName);
+		ReflectorField field = null;
+		for (ReflectorField f : list()) {
+			if (f.getField().getName().equals(fieldName)) {
+				field = f;
+				break;
+			}
+		}
 		if (field == null) {
 			throw new NoSuchFieldException(fieldName);
 		}
-		return new ReflectorField(obj, field);
+		return field;
+	}
+
+	/**
+	 * Gets all {@link Field}s for the given class and all of its superclasses
+	 * 
+	 * @return A {@link HashSet} containing all of the fields of object the
+	 *         Fields instance was instantiated with
+	 */
+	public HashSet<ReflectorField> list() {
+		return list(ALL_FIELDS);
+	}
+
+	/**
+	 * Gets all {@link Field}s for the given class and all of its superclasses
+	 * where {@link Filter#apply(Object)} returns true
+	 * 
+	 * @param filter
+	 *            The filter that determines whether or not a field is added to
+	 *            the return. If {@code null} all fields will be returned.
+	 * @return A {@link HashSet} containing all of the fields of object the
+	 *         Fields instance was instantiated with and that meet the filtering
+	 *         criteria
+	 */
+	public HashSet<ReflectorField> list(Filter<Field> filter) {
+		HashSet<ReflectorField> fields = new HashSet<ReflectorField>();
+
+		if (filter == null) {
+			filter = ALL_FIELDS;
+		}
+
+		Class<?> currentClass = obj.getClass();
+		while (currentClass != null) {
+			for (Field f : currentClass.getDeclaredFields()) {
+				// set accessible to that field values can be accessed
+				f.setAccessible(true);
+
+				// apply filter
+				if (filter.apply(f)) {
+					fields.add(new ReflectorField(obj, f));
+				}
+			}
+			currentClass = currentClass.getSuperclass();
+		}
+		return fields;
 	}
 
 	/**
@@ -56,120 +111,5 @@ public class Fields {
 	 */
 	public static Fields forObj(Object obj) {
 		return new Fields(obj);
-	}
-
-	/**
-	 * Get a field with the specified name in the given class
-	 * 
-	 * @param clazz
-	 *            Class to fetch field from
-	 * @param name
-	 *            Name of the field to get
-	 * @return A field that matches the provided name, {@code null} if there is
-	 *         no field with said name.
-	 */
-	public static Field getField(Class<?> clazz, String name) {
-		for (Field field : getFields(clazz)) {
-			if (field.getName().equals(name)) {
-				return field;
-			}
-		}
-		return null;
-	}
-
-	/**
-	 * Find a field with the given name in the given object and returns fields
-	 * value
-	 * 
-	 * @param <T>
-	 *            Type of the requested field's value
-	 * @param obj
-	 *            Object to find the field in
-	 * @param name
-	 *            Name of the field to look for
-	 * @return The value of the field with the given name. <br>
-	 *         {@code null} will be returned if:
-	 *         <ul>
-	 *         <li>The given object is null</li>
-	 *         <li>The requested field does not exist in the object</li>
-	 *         <li>The requested field is not Public and there is a
-	 *         {@link SecurityManager} in place/</li>
-	 *         <li>The value of the found field is not the same type as T</li>
-	 *         </ul>
-	 */
-	public static Object getFieldValue(Object obj, String name) {
-		if (obj == null) {
-			return null;
-		}
-		Field field = getField(obj.getClass(), name);
-		if (field != null) {
-			try {
-				return field.get(obj);
-			} catch (IllegalArgumentException e) {
-				/*
-				 * object is not an instance of the class that was used to fetch
-				 * the field.
-				 * 
-				 * this should never happen since obj.getClass() was the class
-				 * used to fetch the field
-				 */
-				// TODO: error handling
-			} catch (IllegalAccessException e) {
-				// should not happen since the field object is manually
-				// being set to accessible
-				// TODO: error handling
-			}
-		}
-		return null;
-	}
-
-	/**
-	 * Gets all {@link Field}s for the given class and all of its superclasses.
-	 * 
-	 * @param clazz
-	 *            Class to fetch fields from
-	 * @return A {@link HashSet} containing all of the fields that the given
-	 *         class ( and its superclasses ) has. <br>
-	 * <br>
-	 *         All of the fields returned have had
-	 *         {@link Field#setAccessible(boolean)} called on them.
-	 */
-	public static HashSet<Field> getFields(Class<?> clazz) {
-		return getFields(clazz, ALL_FIELDS);
-	}
-
-	/**
-	 * Gets all {@link Field}s for the given class and all of its superclasses
-	 * where {@link Filter#apply(Object)} returns true
-	 * 
-	 * @param clazz
-	 *            Class to fetch fields from
-	 * @param filter
-	 *            The filter that determines whether or not a field is added to
-	 *            the return
-	 * @return A {@link HashSet} containing all of the fields that the given
-	 *         class ( and its superclasses ) has that meet the filtering
-	 *         criteria. <br>
-	 * <br>
-	 *         All of the fields returned have had
-	 *         {@link Field#setAccessible(boolean)} called on them.
-	 */
-	public static HashSet<Field> getFields(Class<?> clazz, Filter<Field> filter) {
-		HashSet<Field> fields = new HashSet<Field>();
-		if (filter == null) {
-			return fields;
-		}
-
-		Class<?> currentClass = clazz;
-		while (currentClass != null) {
-			for (Field f : currentClass.getDeclaredFields()) {
-				f.setAccessible(true);
-				if (filter.apply(f)) {
-					fields.add(f);
-				}
-			}
-			currentClass = currentClass.getSuperclass();
-		}
-		return fields;
 	}
 }
--- src/test/java/com/zabawaba/reflector/FieldsTest.java
+++ src/test/java/com/zabawaba/reflector/FieldsTest.java
@@ -2,10 +2,8 @@ package com.zabawaba.reflector;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.fail;
 
-import java.lang.reflect.Field;
 import java.util.HashSet;
 
 import org.junit.Test;
@@ -36,41 +34,6 @@ public class FieldsTest {
 		assertNotNull(f);
 	}
 	
-	@Test
-	public void testGetField() throws IllegalArgumentException, IllegalAccessException {
-		Field field = Fields.getField(SampleOne.class, "field1");
-		assertNotNull(field);
-
-		String example = "foo";
-		SampleOne sample = new SampleOne();
-		sample.field1 = example;
-
-		String value = (String) field.get(sample);
-		assertEquals(example, value);
-	}
-
-	@Test
-	public void testGetFieldValue() {
-		String expected = "foo";
-		SampleOne sample = new SampleOne();
-		sample.field1 = expected;
-		Object value = Fields.getFieldValue(sample, "field1");
-		assertEquals(expected, value);
-	}
-
-	@Test
-	public void testGetFieldValue_NullObject() {
-		Object value = Fields.getFieldValue(null, "foo");
-		assertNull(value);
-	}
-
-	@Test
-	public void testGetFieldValue_MissingField() {
-		SampleOne sample = new SampleOne();
-		Object value = Fields.getFieldValue(sample, "missing");
-		assertNull(value);
-	}
-
 	/*
 	 * TODO: figure out why this test messes up cobertura reporting
 	 * 
@@ -86,13 +49,13 @@ public class FieldsTest {
 
 	@Test
 	public void testGetFields() {
-		HashSet<Field> fields = Fields.getFields(SampleOne.class);
+		HashSet<ReflectorField> fields = Fields.forObj(new SampleOne()).list();
 		assertEquals(3, fields.size());
 	}
 
 	@Test
 	public void testGetFields_NoFieldsOnClass() {
-		HashSet<Field> fields = Fields.getFields(Empty.class);
+		HashSet<ReflectorField> fields = Fields.forObj(new Empty()).list();
 
 		// there are 0 fields in the object class
 		assertEquals(0, fields.size());
@@ -100,13 +63,13 @@ public class FieldsTest {
 
 	@Test
 	public void testGetFieldsWithFilter() {
-		HashSet<Field> fields = Fields.getFields(SampleOne.class, Fields.PUBLIC_FIELDS);
+		HashSet<ReflectorField> fields = Fields.forObj(new SampleOne()).list(Fields.PUBLIC_FIELDS);
 		assertEquals(2, fields.size());
 	}
 
 	@Test
 	public void testGetFieldsWithFilter_NullFilter() {
-		HashSet<Field> fields = Fields.getFields(SampleOne.class, null);
-		assertEquals(0, fields.size());
+		HashSet<ReflectorField> fields = Fields.forObj(new SampleOne()).list(null);
+		assertEquals(3, fields.size());
 	}
 }
