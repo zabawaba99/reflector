5c1d80888cf5711b160834809a5d317a85acb27c
Steven Berlanga <zabawaba99@gmail.com>
2015-02-16 20:11:11 -0500

    converting the static methods on Methods to instance methods to reduce the number of parameters that are needed to list/get methods

--- src/main/java/com/zabawaba/reflector/Methods.java
+++ src/main/java/com/zabawaba/reflector/Methods.java
@@ -2,7 +2,6 @@ package com.zabawaba.reflector;
 
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
-import java.util.Arrays;
 import java.util.HashSet;
 
 /**
@@ -46,13 +45,59 @@ public class Methods {
 	 *             If no method exists with the provided name
 	 */
 	public ReflectorMethod get(String methodName) throws NoSuchMethodException {
-		Method method = getMethod(obj.getClass(), methodName);
+		ReflectorMethod method = null;
+		for(ReflectorMethod m:list()){
+			if (m.getMethod().getName().equals(methodName)){
+				method = m;
+			}
+		}
 		if (method == null) {
 			throw new NoSuchMethodException(methodName);
 		}
-		return new ReflectorMethod(obj, method);
+		return method;
 	}
 
+	/**
+	 * Gets all {@link Method}s for the given class and all of its superclasses
+	 * 
+	 * @return A {@link HashSet} containing all of the method of object the
+	 *         Methods instance was instantiated with
+	 */
+	public HashSet<ReflectorMethod> list() {
+		return list(ALL_METHODS);
+	}
+
+	/**
+	 * Gets all {@link Method}s for the given class and all of its superclasses
+	 * where {@link Filter#apply(Object)} returns true
+	 * 
+	 * @param filter
+	 *            The filter that determines whether or not a method is added to
+	 *            the return. If {@code null} all methods will be returned.
+	 * @return A {@link HashSet} containing all of the methods of object the
+	 *         Method instance was instantiated with and that meet the filtering
+	 *         criteria
+	 */
+	public HashSet<ReflectorMethod> list(Filter<Method> filter) {
+		HashSet<ReflectorMethod> methods = new HashSet<ReflectorMethod>();
+
+		if (filter == null) {
+			filter = ALL_METHODS;
+		}
+
+		Class<?> currentClass = obj.getClass();
+		while (currentClass != null) {
+			for (Method m : currentClass.getDeclaredMethods()) {
+				m.setAccessible(true);
+				if (filter.apply(m)) {
+					methods.add(new ReflectorMethod(obj, m));
+				}
+			}
+			currentClass = currentClass.getSuperclass();
+		}
+		return methods;
+	}
+	
 	/**
 	 * Builds a new Methods object with context of the object you want to Reflect
 	 * over
@@ -63,98 +108,5 @@ public class Methods {
 	 */
 	public static Methods forObj(Object obj) {
 		return new Methods(obj);
-	}
-
-	/**
-	 * Get a method with the given name in the given class. If the method you
-	 * are looking for is overloaded, you may not get the method you expect. If
-	 * you are looking for a particular overloaded method, you should use
-	 * {@link #getMethod(Class, String, Class...)}
-	 * 
-	 * @param clazz
-	 *            Class to fetch method from
-	 * @param name
-	 *            The name of the method to return
-	 * @return A method that matches the provided name, {@code null} if there is
-	 *         no method with said name.
-	 */
-	public static Method getMethod(Class<?> clazz, String name) {
-		for (Method m : getMethods(clazz)) {
-			if (m.getName().equals(name)) {
-				return m;
-			}
-		}
-		return null;
-	}
-
-	/**
-	 * Get a method with the specified name in the given class.
-	 * 
-	 * @param clazz
-	 *            Class to fetch method from
-	 * @param name
-	 *            The name of the method to return
-	 * @param params
-	 *            The params of the method to return
-	 * @return A method that matches the provided name, {@code null} if there is
-	 *         no method with said name.
-	 */
-	public static Method getMethod(Class<?> clazz, String name, Class<?>... params) {
-		for (Method m : getMethods(clazz)) {
-			if (m.getName().equals(name) && Arrays.equals(m.getParameterTypes(), params)) {
-				return m;
-			}
-		}
-		return null;
-	}
-
-	/**
-	 * Gets all {@link Method}s for the given class and all of its superclasses.
-	 * 
-	 * @param clazz
-	 *            Class to fetch methods from
-	 * @return A {@link HashSet} containing all of the methods that the given
-	 *         class ( and its superclasses ) has. <br>
-	 * <br>
-	 *         All of the methods returned have had
-	 *         {@link Method#setAccessible(boolean)} called on them.
-	 */
-	public static HashSet<Method> getMethods(Class<?> clazz) {
-		return getMethods(clazz, ALL_METHODS);
-	}
-
-	/**
-	 * Gets all {@link Method}s for the given class and all of its superclasses
-	 * where {@link Filter#apply(Object)} returns true
-	 * 
-	 * @param clazz
-	 *            Class to fetch methods from
-	 * @param filter
-	 *            The filter that determines whether or not a method is added to
-	 *            the return
-	 * @return A {@link HashSet} containing all of the methods that the given
-	 *         class ( and its superclasses ) has that meet the filtering
-	 *         criteria. <br>
-	 * <br>
-	 *         All of the methods returned have had
-	 *         {@link Method#setAccessible(boolean)} called on them.
-	 */
-	public static HashSet<Method> getMethods(Class<?> clazz, Filter<Method> filter) {
-		HashSet<Method> methods = new HashSet<Method>();
-		if (filter == null) {
-			return methods;
-		}
-
-		Class<?> currentClass = clazz;
-		while (currentClass != null) {
-			for (Method m : currentClass.getDeclaredMethods()) {
-				m.setAccessible(true);
-				if (filter.apply(m)) {
-					methods.add(m);
-				}
-			}
-			currentClass = currentClass.getSuperclass();
-		}
-		return methods;
 	}
 }
--- src/test/java/com/zabawaba/reflector/MethodsTest.java
+++ src/test/java/com/zabawaba/reflector/MethodsTest.java
@@ -2,11 +2,8 @@ package com.zabawaba.reflector;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.fail;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.HashSet;
 
 import org.junit.Test;
@@ -24,7 +21,7 @@ public class MethodsTest {
 	}
 	
 	@Test(expected=NoSuchMethodException.class)
-	public void testGet_MissingField() throws NoSuchMethodException {
+	public void testGet_MissingMethod() throws NoSuchMethodException {
 		SampleOne sample = new SampleOne();
 		Methods.forObj(sample).get("i_don't_exist");
 		fail("should have through exception");
@@ -38,66 +35,20 @@ public class MethodsTest {
 	}
 	
 	@Test
-	public void testGetMethod() throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-		Method m = Methods.getMethod(SampleOne.class, "method1");
-		assertNotNull(m);
-
-		String example = "foo";
-		SampleOne sample = new SampleOne();
-		sample.field1 = example;
-
-		String response = (String) m.invoke(sample);
-		assertEquals(example, response);
-	}
-
-	@Test
-	public void testGetMethod_NoMethod() {
-		Method m = Methods.getMethod(SampleOne.class, "missing");
-		assertNull(m);
-	}
-
-	@Test
-	public void testGetMethodWithParams() throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-		Method m = Methods.getMethod(SampleOne.class, "override", String.class, String.class);
-		assertNotNull(m);
-
-		SampleOne sample = new SampleOne();
-		int response = (Integer) m.invoke(sample, "", "");
-		assertEquals(2, response);
-	}
-
-	@Test
-	public void testGetMethodWithParams_NoMethod() throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-		Method m = Methods.getMethod(SampleOne.class, "override", String.class, String.class, String.class);
-		assertNull(m);
-	}
-
-	@Test
-	public void testGetMethods() {
-		HashSet<Method> methods = Methods.getMethods(SampleOne.class);
-		HashSet<Method> parentMethods = Methods.getMethods(Object.class);
-
+	public void testList() {
+		HashSet<ReflectorMethod> methods = Methods.forObj(new SampleOne()).list();
 		assertEquals(18, methods.size());
-		assertEquals(6, methods.size() - parentMethods.size());
 	}
 
 	@Test
-	public void testGetMethods_NoMethodsOnClass() {
-		HashSet<Method> methods = Methods.getMethods(Empty.class);
-
-		// there are 12 methods in the object class
-		assertEquals(12, methods.size());
+	public void testListWithFilter() {
+		HashSet<ReflectorMethod> methods = Methods.forObj(new SampleOne()).list(Methods.PUBLIC_METHODS);
+		assertEquals(14, methods.size());
 	}
 
 	@Test
-	public void testGetMethodsWithFilter() {
-		HashSet<Method> fields = Methods.getMethods(SampleOne.class, Methods.PUBLIC_METHODS);
-		assertEquals(14, fields.size());
-	}
-
-	@Test
-	public void testGetMethodsWithFilter_NullFilter() {
-		HashSet<Method> fields = Methods.getMethods(SampleOne.class, null);
-		assertEquals(0, fields.size());
+	public void testListWithFilter_NullFilter() {
+		HashSet<ReflectorMethod> methods = Methods.forObj(new SampleOne()).list(null);
+		assertEquals(18, methods.size());
 	}
 }
